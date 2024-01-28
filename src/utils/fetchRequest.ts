22770c05e0dfc2db31b4572206fe057257e9c977
coccoto <soprano.lune@gmail.com>
2024-01-28 23:49:22 +0900

    2024-01-28 23:49:22

--- src/utils/fetchRequest.ts
+++ src/utils/fetchRequest.ts
@@ -1,9 +1,14 @@
 export default async <T>(endpoint: string, options: RequestInit): Promise<T> => {
-    const response = await fetch(process.env['HOST'] + endpoint, options)
-    
-    if (! response.ok) {
-        throw new Error(`HTTP Error. Status: ${response.status}`)
+    try {
+        const response = await fetch(process.env['HOST'] + endpoint, options)
+        if (! response.ok) {
+            throw new Error(`HTTP Error. Status: ${response.status}`)
+        }
+        const data = await response.json() as T
+        return data
+
+    } catch (error) {
+        console.error('fetch failed')
+        return {} as T
     }
-    const data = await response.json() as T
-    return data
 }
