d054edc301551ee3f0fd2df44b1f47edb443dceb
coccoto <soprano.lune@gmail.com>
2024-09-29 04:09:27 +0900

    2024-09-29 04:09:27

--- src/utils/getLocation.ts
+++ src/utils/getLocation.ts
@@ -3,7 +3,7 @@ export default async () => {
         return 'http://localhost:18010'
 
     } else if (process.env['ENVIRONMENT'] === 'production') {
-        return 'https://webtoolkit'
+        return 'https://webtoolkit.coccoto.com'
 
     } else {
         console.error('error')
