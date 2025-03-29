26a37cc8db1973f5a900410ef41eaf8105718fb8
coccoto <soprano.lune@gmail.com>
2025-03-30 05:19:32 +0900

    2025-03-30 05:19:32

---
+++ src/app/convert-logical-name/page.tsx
@@ -0,0 +1,22 @@
+// components
+import Main from '@/components/main/main.component'
+// features
+import ConvertLogicalName from '@/features/convert-logical-name/convert-logical-name.component'
+// types
+import { ViewMenuType } from '@/types/ViewMenuType'
+// scripts
+import fetchMenuData from '@/utils/api/fetchMenuData'
+
+export const dynamic = 'force-dynamic'
+
+export default async () => {
+
+    const menuData: ViewMenuType[] = await fetchMenuData()
+
+    return (
+        <Main
+            children={<ConvertLogicalName></ConvertLogicalName>}
+            menuData={menuData}
+        ></Main>
+    )
+}
---
+++ src/features/convert-logical-name/convert-logical-name.component.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+// react
+import React from 'react'
+// components
+import { InputField, InputConfigType } from '@/components/input-field/input-field.component'
+// styles
+import styles from '@/features/convert-logical-name/convert-logical-name.module.sass'
+
+export default () => {
+
+    return (
+        <div></div>
+    )
+}
