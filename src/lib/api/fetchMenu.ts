d50fbcdb69d1bc34c1933e7fc78140c77cbde9cd
coccoto <soprano.lune@gmail.com>
2025-12-20 16:49:08 +0900

    2025-12-20 16:49:08

--- src/lib/api/fetchMenu.ts
+++ src/lib/api/fetchMenu.ts
@@ -1,6 +1,6 @@
 // types
 import { ApiResponseType } from '@/types/ApiType'
-import { MenuType } from '@/types/MenuType'
+import { MenuType, initMenuType } from '@/types/MenuType'
 // lib
 import fetchRequest from '@/lib/api/fetchRequest'
 
@@ -8,12 +8,12 @@ export const fetchMenuList = async () => {
     const response = await fetchRequest<ApiResponseType<MenuType[]>>(`/api/menu-list`, {
         method: 'get'
     })
-    return response.result as MenuType[]
+    return response.result || []
 }
 
 export const fetchMenu = async (menuId: string) => {
     const response = await fetchRequest<ApiResponseType<MenuType>>(`/api/menu-list/${menuId}`, {
         method: 'get'
     })
-    return response.result as MenuType
+    return response.result || initMenuType()
 }
