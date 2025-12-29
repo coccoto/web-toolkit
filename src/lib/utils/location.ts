55561b3523accf0e1479cbf1b1aa6747279e9fe4
coccoto <soprano.lune@gmail.com>
2025-12-30 02:33:38 +0900

    2025-12-30 02:33:38

--- src/lib/utils/location.ts
+++ src/lib/utils/location.ts
@@ -1,13 +1,13 @@
 
 export default async () => {
     if (process.env['NEXT_PUBLIC_ENVIRONMENT'] === 'development') {
-        return 'http://localhost:18010'
+        return `http://localhost:${process.env.NEXT_PUBLIC_PORT}`
 
     } else if (process.env['NEXT_PUBLIC_ENVIRONMENT'] === 'production') {
         return 'https://web-toolkit.coccoto.com'
 
     } else {
-        console.log('An error occurred in getLocation.')
+        console.log('An error occurred in location.')
         return null
     }
 }
